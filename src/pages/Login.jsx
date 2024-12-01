2e84748de495eda7acd71cd1493fb00156b334ad
hosseinrz <hossein96rz@gmail.com>
2024-12-01 11:23:02 +0330

    color-pallete added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ function Login() {
   return (
     <div className="flex h-full items-center justify-center px-1 py-2">
       <div className="login-gradient h-full w-1/2"></div>
-      <div className="w-1/2"></div>
+      <div className="w-1/2">asd</div>
     </div>
   );
 }
