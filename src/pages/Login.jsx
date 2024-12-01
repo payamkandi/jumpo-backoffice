7555637d1705e1404a5234d8d82b823a5aff4da3
hosseinrz <hossein96rz@gmail.com>
2024-12-01 10:55:00 +0330

    login image added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,10 @@
 function Login() {
-  return <div className="p-10">Login Page</div>;
+  return (
+    <div className="flex h-full items-center justify-center px-1 py-2">
+      <div className="login-gradient h-full w-1/2"></div>
+      <div className="w-1/2"></div>
+    </div>
+  );
 }
 
 export default Login;
